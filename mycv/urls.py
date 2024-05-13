b2a3dbd85f9786b2c9c7c1237791826b99a7d112
Orçun Karaarslan <138437536+Orcunjrr@users.noreply.github.com>
2024-05-13 21:50:08 +0300

    application of general settings

--- mycv/urls.py
+++ mycv/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import index
 from .views import contact
 
-urlpatterns = {
+
+urlpatterns = [
     path('', index, name='index'),
     path('contact/', contact, name='contact'),
-}
+]
--- mycv/views.py
+++ mycv/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
+from mycv.models import GeneralSetting
 
 
 def index(request):
-    return render(request, 'index.html')
+    site_title = GeneralSetting.objects.get(name='site_title').parameter
+    site_keywords = GeneralSetting.objects.get(name='site_keywords').parameter
+    site_description = GeneralSetting.objects.get(name='site_description').parameter
+    site_myname = GeneralSetting.objects.get(name='site_myname').parameter
+    site_aboutme = GeneralSetting.objects.get(name='site_aboutme').parameter
+    site_bronse = GeneralSetting.objects.get(name='site_bronse').parameter
+    site_gold = GeneralSetting.objects.get(name='site_gold').parameter
+    site_diamond = GeneralSetting.objects.get(name='site_diamond').parameter
+
+    context = {
+        'site_title': site_title,
+        'site_keywords': site_keywords,
+        'site_description': site_description,
+        'site_myname': site_myname,
+        'site_aboutme': site_aboutme,
+        'site_bronse': site_bronse,
+        'site_gold': site_gold,
+        'site_diamond': site_diamond,
+    }
+    return render(request, 'index.html', context=context)
 
 def contact(request):
     return render(request, 'contact.html')
