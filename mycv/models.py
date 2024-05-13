b0af3c04c8a91d5b81c57de2c1d9e06b990a2989
Orçun Karaarslan <138437536+Orcunjrr@users.noreply.github.com>
2024-05-13 14:06:28 +0300

    add general settings

--- mycv/models.py
+++ mycv/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
+
 # Create your models here.
+
+class GeneralSetting(models.Model):
+    name = models.CharField(
+        default='',
+        max_length=254,
+        blank='True',
+        verbose_name='Name',
+        help_text='This is variable of this setting.'
+    )
+    description = models.CharField(
+        default='',
+        max_length=254,
+        blank='True',
+        verbose_name='Description'
+    )
+    parameter = models.CharField(
+        default='',
+        max_length=254,
+        blank='True',
+        verbose_name='Parameter'
+    )
+    updated_date = models.DateTimeField(
+        blank='True',
+        auto_now='True',
+        verbose_name='Updated Date'
+    )
+    created_date = models.DateTimeField(
+        blank='True',
+        auto_now_add='True',
+        verbose_name='Created Date'
+    )
+
+    def __str__(self):
+        return f'General Setting: {self.name}'
+
+
+    class Meta:
+        verbose_name = 'General Setting'
+        verbose_name_plural = 'General Settings'
+        ordering = ('name', )
